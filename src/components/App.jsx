955c4faa908f7df8b3d2050810163dabc8e2a2e9
Sergey Kravchenko <skravchenko223@gmail.com>
2023-07-02 11:17:09 +0200

    create Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import Button from './button/Button';
+import ImageGallery from './imagegallery/Imagegallery';
+import ImageGalleryItem from './imagegalleryitem/Imagegalleryitem';
+import Loader from './loader/Loader';
+import Modal from './modal/Modal';
+import Searchbar from './searchbar/Searchbar';
+
+export class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Searchbar />
+        <ImageGallery />
+        <ImageGalleryItem />
+        <Loader />
+        <Button />
+        <Modal></Modal>
+      </div>
+    );
+  }
+}
